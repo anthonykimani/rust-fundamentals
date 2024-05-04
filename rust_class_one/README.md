5c42f4caddadc2e05516b4e31fe8a4ffcbec99cf
anthonykimani <kimaniantoni@gmail.com>
2024-05-04 13:56:33 +0300

    feat: converting strings, tuples, enums, attributes

--- rust_class_one/README.md
+++ rust_class_one/README.md
@@ -2,3 +2,7 @@
 
 - We learnt signed integers, unsigned integers, floats, char, Strings, Functions.
 - How to use cargo for rust development
+
+### Saturday
+
+- we learnt to convert strings, tuples, enums, attributes
--- rust_class_one/src/main.rs
+++ rust_class_one/src/main.rs
@@ -34,9 +34,49 @@ fn main() {
 
     let text_2: String = String::from("Rust");
     println!("We are learning {} for Blockchain Development", text_2);
+
+    // convert to string
+    let number_one: i32 = 101;
+    let mut number_one_string: String = number_one.to_string();
+
+    number_one_string.push_str("people in the room");
+    println!("Hey {number_one_string}");
+
+    let input_one: &str = "21";
+    let input_number: i32 = input_one.parse().expect("was expecting an integer");
+
+    println!("I'm finally {input_number}");
+
+    // let _first_input: i32 = 200;
+    // let _second_input:i16 = first_input as i16;
+
+    // println!("Big cup to ")
     
 
     sum(3,5);
+
+    // tuples in rust
+    let (data, status, reason) = do_sum_maths(45,22);
+
+    println!("Sum = {data} was it success {status} reason: {reason}");
+
+    // enums in rust
+    const age: i32 = 13;
+
+    let ages_2: i32 = 15;
+
+    #[derive(Debug)] // An Example of an attribute in rust
+    enum STATUS {
+        ADULT, CHILD
+    }
+
+    let user_status: STATUS = if age > 18 {
+        STATUS::ADULT
+    } else {
+        STATUS::CHILD
+    };
+
+    println!("The User Status is {:?}", user_status);
 
 }
 
@@ -45,4 +85,8 @@ fn sum(number_one: u8, number_two: u8) -> u8 {
     let sum: u8 = number_one + number_two;
     println!("The Sum of number_one and number_two is {}", sum);
     return sum;
+}
+
+fn do_sum_maths(par_1: i32, par_2: i32) -> (i32, bool, String) {
+    return (0, false, "Failed for some reason".to_string())
 }
